f6bc9cb03df669a0912f265992627a1b1ff5f244
Patrick Miller <143756067+CozyOkami@users.noreply.github.com>
2023-09-24 04:53:10 +0000

    added setters and getters

--- lab04/user.cpp
+++ lab04/user.cpp
@@ -8,4 +8,23 @@ class User {
         std::string email;
         std::vector<User*> friendList;
 
+    public:
+        User(const std::string& name, const std::string& userEmail)
+            : userName(name), email(userEmail) {}
+
+        void setUserName(const std::string& name) {
+            userName = name;
+    }
+
+        std::string getUserName() const {
+            return userName;
+    }
+
+        void setEmail(const std::string& userEmail) {
+            email = userEmail;
+    }
+
+        std::string getEmail() const {
+            return email;
+    }
 }
